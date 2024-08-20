0937a507523a76656da32b851ffb9cc4b12a1318
Vicky <126154136+vickyxiz@users.noreply.github.com>
2024-08-20 21:32:25 +0530

    Update script.js

--- Restaurant Landing Page/script.js
+++ Restaurant Landing Page/script.js
@@ -1,6 +1,6 @@
 
 const body = document.querySelector("body"), 
-    loader = document.querySelector(".loader-wrap"),
+    loader = document.querySelector(".loader-wrap"), 
     links = document.querySelectorAll('a[href="#"]'),
     nav = document.querySelector("header nav"),
     navToggle = document.querySelector("header nav .toggle"),
